e04b9b9680dc07dd1a69be74e57e9dc2a9fed007
Valeria Serci <balerty84@gmail.com>
2023-02-07 19:21:09 +0000

    change array of array to array of hashes

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
 students = [
-  ['Dr. Hannibal Lecter', :november],
-  ['Darth Vader', :november],
-  ['Nurse Ratched', :november],
-  ['Michael Corleone', :november],
-  ['Alex DeLarge', :november],
-  ['The Wicked Witch of the West', :november],
-  ['Terminator', :november],
-  ['Freddy Krueger', :november],
-  ['The Joker', :november],
-  ['Joffrey Baratheon', :november],
-  ['Norman Bates', :november]
+  {name: 'Dr. Hannibal Lecter', cohort: :november},
+  {name: 'Darth Vader', cohort: :november},
+  {name: 'Nurse Ratched', cohort: :november},
+  {name: 'Michael Corleone', cohort: :november},
+  {name: 'Alex DeLarge', cohort: :november},
+  {name: 'The Wicked Witch of the West', cohort: :november},
+  {name: 'Terminator', cohort: :november},
+  {name: 'Freddy Krueger', cohort: :november},
+  {name: 'The Joker', cohort: :november},
+  {name: 'Joffrey Baratheon', cohort: :november},
+  {name: 'Norman Bates', cohort: :november}
 ]
 
 def print_header
@@ -30,3 +30,5 @@ end
 print_header()
 print(students)
 print_footer(students)
+
+
