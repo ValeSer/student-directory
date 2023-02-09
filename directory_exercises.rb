8d0c73d0f0e0164c0a85c15027b35784ba84ffdf
Valeria Serci <balerty84@gmail.com>
2023-02-09 12:54:36 +0000

    change loop to be a while

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -17,11 +17,11 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student,index|
-    if student[:name].length < 12
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  i = 0
+  while i < students.length
+      puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+      i +=1
     end
-  end
 end
 
 def print_footer(students)
