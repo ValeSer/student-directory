389e060d6579f33fa6b8a045bba5f47338349687
Valeria Serci <balerty84@gmail.com>
2023-02-10 15:53:47 +0000

    implement interactive menu

--- interactive_menu.rb
+++ interactive_menu.rb
@@ -1,5 +1,19 @@
 def interactive_menu
   loop do
-    
+    puts "1. Input the student"
+    puts "2. Show the student"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
   end
 end
