38fc99d90659e610a8ee83b1c9a0bd5b7cbdf6d6
Valeria Serci <balerty84@gmail.com>
2023-02-10 10:47:56 +0000

    fix what it gets printed and get the cohort as info

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,27 +1,29 @@
 require 'io/console'
 
 def input_students
-  puts "Please enter a students' name"
+  puts "Please enter a students' name and cohort"
   students = []
   name = gets.chomp
   
   while !name.empty? do
+    cohort = gets.chomp
+    cohort.empty? ? cohort == :november : cohort.to_sym
     puts "Please enter the hobbies of the student"
-  hobbies = gets.chomp
-  puts "Please enter the country of birth of the student"
-  country_of_birth = gets.chomp
-  puts "Please enter the height of the student"
-  height = gets.chomp
-    students << {name: name, cohort: :november, :hobbies => hobbies, "country of birth" => country_of_birth , :height => height}
-     if students.length == 1
-      puts "Now we have #{students.count} student"
-     else
-      puts "Now we have #{students.count} students"
-     end
-    puts "Please enter another name or just hit return twice to exit"
-    name = gets.chomp
-  end
-  students
+    hobbies = gets.chomp
+    puts "Please enter the country of birth of the student"
+    country_of_birth = gets.chomp
+    puts "Please enter the height of the student"
+    height = gets.chomp
+      students << {name: name, cohort: cohort, :hobbies => hobbies, "country of birth" => country_of_birth , :height => height}
+      if students.length == 1
+        puts "Now we have #{students.count} student"
+      else
+        puts "Now we have #{students.count} students"
+      end
+      puts "Please enter another name or just hit return twice to exit"
+      name = gets.chomp
+    end
+    students
 end
 
 def print_header
@@ -32,7 +34,7 @@ end
 def print(students)
   i = 0
   while i < students.length
-      puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort #{students[i][:hobbies]})"
+      puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort, hobbies are: #{students[i][:hobbies]}, they come from #{students[i]["country of birth"]} and are #{students[i][:height]}cm tall)"
       i +=1
     end
 end
