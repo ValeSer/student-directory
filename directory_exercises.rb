a69c450a40edde560a8d4cdf067a31615d7322af
Valeria Serci <balerty84@gmail.com>
2023-02-10 15:55:54 +0000

    transfer interactive menu to main ex

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -48,8 +48,25 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great student" + (students.length == 1 ? '' : 's')
 end
 
-
-
+def interactive_menu
+  loop do
+    puts "1. Input the student"
+    puts "2. Show the student"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
 
 
 students = input_students
